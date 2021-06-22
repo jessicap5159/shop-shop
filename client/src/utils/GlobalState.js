d7c3be891d3d4a43b6642b841cff7ff846ef6457
jessicap5159 <jessicaj5159@gmail.com>
2021-06-22 13:27:56 -0700

    refactored components to use global state

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,27 @@
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from './reducers';
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state,dispatch] = useProductReducer({
+        products: [],
+        categories: [],
+        currentCategory: ','
+    });
+    // use this to confirm it works
+    console.log(state);
+    return <Provider value={{state,dispatch}} {...props} />;
+};
+
+// to be used by components that need the data <Store Provider> provides
+// custom React Hook - when we execute this function from within a component,
+// we will receive the [state,dispatch] data our StoreProvider provider manages for us
+// this means that any component with access to StoreProvider can use any data
+// in our global state container or update it using the dispatch function
+const useStoreContext = () => {
+    return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
