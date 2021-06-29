97a268a0278fb39c727162a02be019b5922096e2
jessicap5159 <jessicaj5159@gmail.com>
2021-06-29 16:28:06 -0700

    moved store into index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore } from 'redux';
+import reducer from '../src/utils/reducers';
+
+const store = createStore(reducer);
+console.log('I am the store current state', store.getState());
+
+export default store;
+
+// Store
+
+// Action
+
+// Reducer
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- client/src/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from 'redux';
-import rootReducer from './reducer';
-
-const store = createStore(rootReducer);
-console.log('I am the current state', store.getState());
-
-export default store;
