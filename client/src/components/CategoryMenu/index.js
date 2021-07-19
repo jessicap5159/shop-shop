7078a21fed2d1a21b681da8f687dbeee45999758
jessicap5159 <jessicaj5159@gmail.com>
2021-07-18 18:29:02 -0700

    updated code for categorymenu

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,59 +1,22 @@
-// import React, { useEffect } from 'react';
-// import { useQuery } from '@apollo/react-hooks';
-// import { useStoreContext } from '../../utils/GlobalState';
-// import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
-// import { QUERY_CATEGORIES } from '../../utils/queries';
-
-// function CategoryMenu({ setCategory }) {
-//  const [state, dispatch] = useStoreContext();
-
-//  const { categories } = state;
-
-//  const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-
-//  useEffect(() => {
-//    // if categoryData exists or has changed from the response of useQuery, then run dispatch()
-//    if (categoryData) {
-//      // execute dispatch function with our action object indicating the type of action and the data to set our state for categories to
-//      dispatch({
-//        type: UPDATE_CATEGORIES,
-//        categories: categoryData.categories
-//      });
-//    }
-//  }, [categoryData, dispatch]);
-//   return (
-//     <div>
-//       <h2>Choose a Category:</h2>
-//       {categories.map((item) => (
-//         <button
-//           key={item._id}
-//           onClick={() => {
-//             setCategory(item._id);
-//           }}
-//         >
-//           {item.name}
-//         </button>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default CategoryMenu;
-
 import React, { useEffect } from 'react';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
+// import { useStoreContext } from "../../utils/GlobalState";
 import { useQuery } from '@apollo/client';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_CART_QUANTITY, UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
+import { useSelector, useDispatch, } from 'react-redux';
 import { QUERY_CATEGORIES } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 
-
 function CategoryMenu() {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector((state) => {
+    return state
+  });
+  
 
-  const { categories } = state;
+  const dispatch = useDispatch();
 
   const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+  const { categories } = state;
 
   useEffect(() => {
     if (categoryData) {
